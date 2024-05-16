2fe970de5b48222f1017b28a563520796dc46e8c
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2024-05-15 22:42:59 -0300

    Aprimorando do Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,55 @@ form.addEventListener("submit", function(event){
     //toFixed para casas decimais , e realizando o calculo do imc
     const imc = (weight / (height * height)).toFixed(2);
 
-    const value = document.getElementById('value');
-    let description = ''
+    console.log(imc);
 
-    //removendo a classe hidden para aparecer as informações
+    const value = document.getElementById('value');
+    let description = '';
+
+
+    value.classList.add('attention');
+    
+    //tirando o hidden
     document.getElementById('infos').classList.remove('hidden');
+
+    if (imc < 18.5){
+        description = 'Cuidado! Você está abaixo do peso!';
+    } 
+
+    else if (imc >= 18.5 && imc <= 25) {
+        description = "Você está no peso ideal!";
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    }
+
+    else if (imc > 25 && imc <= 30) {
+        description = "Cuidado! Você está com sobrepeso!";
+    }
+
+    else if (imc > 30 && imc <= 35) {
+        description = "Cuidado! Você está com obesidade moderada!";
+    }
+
+    else if (imc > 35 && imc <= 40) {
+        description = "Cuidado! Você está com obesidade severa!";
+    }
+
+    else {
+        description = "Cuidado! Você está com obesidade morbida!";
+    }
+
+    value.textContent = imc.replace('.', ',');
+    document.getElementById('description').textContent = description;
 
 
 });
+
+form.addEventListener('keypress', function(event) {
+    if (event.key === 'press') {
+        document.getElementById('#calculate').click();
+    }
+});
+
 
 
 // alternar entre modo escuro e claro
